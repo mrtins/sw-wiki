b5c60aadacdfaf148d88f7ff08951cd19505db76
Vitor Martins <viitormartins@hotmail.com>
2017-12-13 16:48:26 -0200

    route for startships

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import { CharactersComponent } from './characters/characters.component';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { StarshipsComponent } from './starships/starships.component';
 
 const appRoutes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'characters', component: CharactersComponent },
+  { path: 'starships', component: StarshipsComponent },
   { path: '**', component: HomeComponent }
 ];
 
