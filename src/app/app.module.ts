8f2459a8d778fde6861e292204a433306d173fee
Vitor Martins <viitormartins@hotmail.com>
2017-12-13 16:48:18 -0200

    startships service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,32 @@
-import { CharactersService } from './characters/characters.service';
-import { routing } from './app.routes';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { CharactersComponent } from './characters/characters.component';
 import { HttpModule } from '@angular/http';
+
+import { routing } from './app.routes';
+import { AppComponent } from './app.component';
+
+import { HomeComponent } from './home/home.component';
+
+import { CharactersComponent } from './characters/characters.component';
+import { CharactersService } from './characters/characters.service';
+
+import { StarshipsComponent } from './starships/starships.component';
+import { StarshipsService } from './starships/starships.service';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    CharactersComponent
+    CharactersComponent,
+    StarshipsComponent
   ],
   imports: [
     BrowserModule,
     HttpModule,
     routing
   ],
-  providers: [CharactersService],
+  providers: [CharactersService, StarshipsService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
